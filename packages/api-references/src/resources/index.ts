1750b168b8a1e1e38a40e4a620316ebc98e1f7b9
socram03 <marcosjgs03@gmail.com>
2022-09-25 20:16:08 -0400

    fix: update exports

--- packages/api-references/src/resources/index.ts
+++ packages/api-references/src/resources/index.ts
@@ -1,1 +1,6 @@
-export {};
+export * from './channel-resource';
+export * from './doc-resource';
+export * from './forum-resource';
+export * from './member-resource';
+export * from './message-resource';
+export * from './user-resource';
