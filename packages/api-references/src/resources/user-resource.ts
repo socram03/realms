6589b5eff9074c0ec079c68460654ff2368c2193
socram03 <marcosjgs03@gmail.com>
2022-09-25 20:08:34 -0400

    feat: user

--- packages/api-references/src/resources/user-resource.ts
+++ packages/api-references/src/resources/user-resource.ts
@@ -1,1 +1,27 @@
-export {};
+/** @link https://www.guilded.gg/docs/api/members/UserSummary */
+export interface GuildedUserSummary {
+    /** The ID of the user */
+    id: string;
+    /** 
+     * The type of user.
+     * If this property is absent, it can assumed to be of type user 
+     */
+    type?: GuildedUserTypes;
+    /** The name of the user */
+    name: string;
+    /** The avatar image associated with the user */
+    avatar?: string;
+}
+
+/** @link https://www.guilded.gg/docs/api/members/User */
+export interface GuildedUser extends GuildedUserSummary {
+    /** The banner image associated with the user */
+    banner?: string;
+    /** The ISO 8601 timestamp that the user was created at */
+    createdAt: string;
+}
+
+export enum GuildedUserTypes {
+    user = 'user',
+    bot = 'bot'
+}
